f9f4953625b1c8c0a26d210975e1b49e22407b90
Nishant Surwade <84285077+Nishant982001@users.noreply.github.com>
2025-03-25 22:34:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,11 +30,11 @@ with main:
     with gmail:
         st.button("","https://mail.google.com/mail/u/0/#sent?compose=CllgCKCFSmddMrZBLdrWLDRkBHTjKqZzGQmQwhNwBshRTZgTMSCGQKXMcWsDWdxbcxZHNQPqcBB",use_container_width=True,icon=":material/mail:")
     with linked:
-        st.link_button("linked-in","https://www.linkedin.com/in/nishantsurwade8314/",use_container_width=True)
+        st.link_button("LINKEDIN","https://www.linkedin.com/in/nishantsurwade8314/",use_container_width=True)
     with insta:
-        st.button("insta", use_container_width=True)
+        st.button("INSTA","https://www.instagram.com/nishant_surwade.exe/", use_container_width=True)
     with github:
-        st.link_button("github","https://github.com/Nishant982001", use_container_width=True)
+        st.link_button("GITHUB","https://github.com/Nishant982001", use_container_width=True)
     with tweet:
         st.button("X","https://x.com/INishantSurwade", use_container_width=True)
 with starter:
